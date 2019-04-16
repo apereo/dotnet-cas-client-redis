19e4d32eba53ddc80f8688a3de2e9d106f1445b9
Jason Kanaris <phantomtypist@hotmail.com>
2019-04-15 23:41:39 -0400

    Added initial version of project.

---
+++ src/DotNetCasClient.Redis/State/RedisClientManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using StackExchange.Redis;
+
+namespace DotNetCasClient.State
+{
+    /// <summary>
+    /// Represents a singleton implementation of StackExchange.Redis ConnectionMultiplexer.  See https://github.com/StackExchange/StackExchange.Redis
+    /// </summary>
+    public class RedisClientManager
+    {
+        private static readonly Lazy<RedisClientManager> lazy = new Lazy<RedisClientManager>(() => new RedisClientManager());
+
+        /// <summary>
+        /// Gets the instance of RedisClientManager.
+        /// </summary>
+        public static RedisClientManager Instance { get { return lazy.Value; } }
+
+        private ConnectionMultiplexer _redis;
+
+        private RedisClientManager()
+        {
+            _redis = ConnectionMultiplexer.Connect("localhost");
+        }
+
+        /// <summary>
+        /// Returns the object representing the Redis database implemented by StackExchange.Redis.
+        /// </summary>
+        /// <returns></returns>
+        public IDatabase GetDatabase()
+
+        {
+            return _redis.GetDatabase(int.Parse(ConfigurationManager.AppSettings["cas:Redis:DatabaseNumber"]));
+        }
+    }
+}
